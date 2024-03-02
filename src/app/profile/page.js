25a6c5e087c0351561fe96dd9880c434dd25cfac
Vinicius Konkolics Possobom <vpossobom@gatech.edu>
2024-03-02 18:18:17 -0500

    Created basic profile page structure

---
+++ src/app/profile/page.js
@@ -0,0 +1,12 @@
+import styles from "../page.module.css";
+
+export default function Home() {
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.header}>
+        <h1>Profile Page</h1>
+      </div>
+    </main>
+  );
+}
