ad9e3cb746baeaa98b3eb2ba474d8dee456f86ad
Vinicius Konkolics Possobom <vpossobom@gatech.edu>
2024-03-02 19:17:49 -0500

    Added radixUI functionality

--- src/app/quiz/page.js
+++ src/app/quiz/page.js
@@ -2,15 +2,23 @@
 
 import styles from "../page.module.css";
 import { useSearchParams } from "next/navigation"
+import { Button } from '@radix-ui/themes';
+
 export default function Quiz() {
     const searchParams = useSearchParams()
     const id = searchParams.get('id')
-    
+    console.log("id")
+    const quizName = "Harry Potter"
+
+
     return (
         <main className={styles.main}>
-        <div className={styles.header}>
-            <h1>{id}</h1>
-        </div>
+            <div className={styles.header}>
+                <h1>{quizName}</h1>
+            </div>
+            <Button>
+                Start Game
+            </Button>
         </main>
     );
 }
