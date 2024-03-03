db77ce37c0bc43324b984e8a9ae81d6049698308
Vinicius Konkolics Possobom <vpossobom@gatech.edu>
2024-03-02 20:25:12 -0500

    Added loading bar to looking for players page

--- src/app/quiz/play/page.js
+++ src/app/quiz/play/page.js
@@ -1,14 +1,30 @@
 'use client'
-
-
+import React from 'react';
 import { useSearchParams } from "next/navigation"
+import * as Progress from '@radix-ui/react-progress';
+import './styles.css';
 
 export default function PlayQuiz() {
+    const [progress, setProgress] = React.useState(13);
+
+    React.useEffect(() => {
+        const timer = setTimeout(() => setProgress(66), 500);
+        return () => clearTimeout(timer);
+    }, []);
+
     const searchParams = useSearchParams()
     const id = searchParams.get('quizId')
+
     return (
         <main>
             Looking for players...
+            <Progress.Root className="ProgressRoot" value={progress}>
+                <Progress.Indicator
+                    className="ProgressIndicator"
+                    style={{ transform: `translateX(-${100 - progress}%)` }}
+                />
+            </Progress.Root>
+            
         </main>
     );
 
