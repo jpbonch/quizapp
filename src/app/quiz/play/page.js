4c0a5e7481217d930c85393c3edef04f5349c8e7
jpbonch <jp.bonchristiano@gmail.com>
2024-03-04 18:34:26 -0500

    Basic connections

--- src/app/quiz/play/page.js
+++ src/app/quiz/play/page.js
@@ -2,16 +2,23 @@
 import React from 'react';
 import { useSearchParams } from "next/navigation"
 import * as Progress from '@radix-ui/react-progress';
+import { socket } from '../../socket';
+import { useEffect, useState } from 'react';
 import './styles.css';
 
 export default function PlayQuiz() {
+    const [isConnected, setIsConnected] = useState(socket.connected);
     const [progress, setProgress] = React.useState(13);
-    const [matchFound, setMatchFound] = React.useState(true);
+    const [matchFound, setMatchFound] = React.useState(false);
 
     React.useEffect(() => {
         const timer = setTimeout(() => setProgress(66), 500);
         return () => clearTimeout(timer);
     }, []);
+
+    useEffect(() => {
+        socket.on('found', () => {setMatchFound(true)});
+     }, []);
 
     const searchParams = useSearchParams()
     const id = searchParams.get('quizId')
