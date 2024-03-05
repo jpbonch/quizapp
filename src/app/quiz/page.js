04a74779f4a807587769313740e55528515f5e27
Vinicius Konkolics Possobom <vpossobom@gatech.edu>
2024-03-04 19:58:28 -0500

    Added basic ranking functionality

--- src/app/quiz/page.js
+++ src/app/quiz/page.js
@@ -1,16 +1,37 @@
 'use client'
 
+import React from 'react';
 import styles from "../page.module.css";
 import { useSearchParams } from "next/navigation"
-import { Button, Grid } from '@radix-ui/themes';
+import { Button, Grid, Table, Text } from '@radix-ui/themes';
 import Link from "next/link"
 
 export default function Quiz() {
-    const searchParams = useSearchParams()
-    const id = searchParams.get('quizId')
-    console.log(id)
-    const quizName = "Harry Potter"
+    const searchParams = useSearchParams();
+    const id = searchParams.get('quizId');
+    const [rankingData, setRankingData] = React.useState([{rank: "1", user:"Vini", xp: "123"}])
+    console.log(id);
+    const quizName = "";
+    const rows = rankingData.map(data => 
+            <Table.Row>
+                <Table.RowHeaderCell>{data.rank}</Table.RowHeaderCell>
+                <Table.Cell>{data.user}</Table.Cell>
+                <Table.Cell>{data.xp}</Table.Cell>
+            </Table.Row>
+        )
 
+    // const renderTableRows = () => {
+    //     let rows = [];
+    //     for (let i = 0; i < rankingData.length; i++) {
+    //         rows.push(
+    //             <Table.Row>
+    //                 <Table.RowHeaderCell>{Object.keys(rankingData[i])[0]}</Table.RowHeaderCell>
+    //                 <Table.Cell>{Object.values(rankingData[i])[0]}</Table.Cell>
+    //             </Table.Row>
+    //         )
+    //     }
+    //     return rows;
+    // }
 
     return (
         <main className={styles.main}>
@@ -29,6 +50,22 @@ export default function Quiz() {
                 <Button>
                     Play with a friend
                 </Button>
+
+                <Text> Rankings </Text>
+                <Table.Root>
+                    <Table.Header>
+                        <Table.Row>
+                        <Table.ColumnHeaderCell>Rank</Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell>User</Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell>XP</Table.ColumnHeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+
+                    <Table.Body>
+                        {rows}
+                    </Table.Body>
+                </Table.Root>
+
             </Grid>
         </main>
     );
